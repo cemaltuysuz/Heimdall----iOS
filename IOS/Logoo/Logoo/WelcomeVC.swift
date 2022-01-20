c1e15280ccb0c3b34af50b0811710824cdabc3cc
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2022-01-20 11:06:03 +0300

    IOS | init project, created some folder

---
+++ IOS/Logoo/Logoo/WelcomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeVC.swift
+//  Logoo
+//
+//  Created by cemal tüysüz on 20.01.2022.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
