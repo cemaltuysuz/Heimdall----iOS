a64e2fcb401944fd83bfd6ce30e574bb6d629547
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2022-02-08 13:14:16 +0300

    IOS | Autofill enable for login (usermail coming from register vc)

--- IOS/Logoo/Logoo/login-module/view/LoginVC.swift
+++ IOS/Logoo/Logoo/login-module/view/LoginVC.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var loginUserMail: UITextField!
     var incomingMail:String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let mail = incomingMail {
+            self.loginUserMail.text = mail
+        }
 
+    }
+    @IBAction func loginButton(_ sender: Any) {
+        
     }
 }
