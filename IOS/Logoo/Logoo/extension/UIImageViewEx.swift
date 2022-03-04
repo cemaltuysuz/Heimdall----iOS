a15ff6f6015523fa45b62a0b1c96e643947efabc
Cemal Tuysuz <cemal.tuysuz@topkapidanismanlik.com.tr>
2022-03-04 14:20:24 +0300

    IOS | Revised setImage function (image cache and process etc.)

--- IOS/Logoo/Logoo/extension/UIImageViewEx.swift
+++ IOS/Logoo/Logoo/extension/UIImageViewEx.swift
@@ -13,8 +13,29 @@ import UIKit
 
 extension UIImageView {
     
-    func setImage(url:URL){
-        self.kf.setImage(with: url)
+    func setImage(urlString:String){
+        let url = URL(string: urlString)
+        let processor = DownsamplingImageProcessor(size: self.bounds.size)
+                     |> RoundCornerImageProcessor(cornerRadius: 20)
+        self.kf.indicatorType = .activity
+        self.kf.setImage(
+            with: url,
+            placeholder: nil,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+        {
+            result in
+            switch result {
+            case .success(let value):
+                print("Task done for: \(value.source.url?.absoluteString ?? "")")
+            case .failure(let error):
+                print("Job failed: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
